68da72aeae10822c3e67faa425834e2c9a170dd3
Artgnz <arturognz@ciencias.unam.mx>
2024-03-17 11:50:35 -0600

    Correcion en app.py y renombramiento de carpeta contollers

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 
 from alchemyClasses import db
-from contollers.PrimerControlador import mi_primer_blueprint
-from contollers.ControllerAlumno import alumno_blueprint
+from controllers.PrimerControlador import mi_primer_blueprint
+from controllers.ControllerAlumno import alumno_blueprint
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ferfong:Develooper123!@localhost:3306/ing_soft'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://ferfong:Developer123!@localhost:3306/ing_soft'
 app.config.from_mapping(
     SECRET_KEY='dev'
 )
