c67b43dcbda9f794b7981b12d92586e69c1e8e55
Artgnz <arturognz@ciencias.unam.mx>
2024-03-18 10:24:22 -0600

    Agrega alchemy classes.

---
+++ flaskProject/alchemyClasses/Renta.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
+from alchemyClasses import db
+from sqlalchemy.orm import relationship
+from alchemyClasses.Usuario import Usuario
+from alchemyClasses.Pelicula import Pelicula
+
+class Renta(db.Model):
+
+    __tablename__ = 'rentar'
+    idRentar = Column(Integer, primary_key=True)
+    idUsuario = Column(Integer, ForeignKey(Usuario.idUsuario))
+    idPelicula = Column(Integer, ForeignKey(Pelicula.idPelicula))
+    fecha_renta = Column(DateTime)
+    dias_de_renta = Column(Integer, nullable=True, default=4)
+    estatus = Column(Boolean, nullable=True, default=0)
+
+    def __init__(self, id_usuario, id_pelicula, fecha_renta, dias_de_renta=5, estatus=0):
+        self.idUsuario = id_usuario
+        self.idPelicula = id_pelicula
+        self.fecha_renta = fecha_renta
+        self.dias_de_renta = dias_de_renta
+        self.estatus = estatus
+
+    def __str__(self):
+        cadena = f'ID Usuario: {self.idUsuario}\nID Pelicula: {self.idPelicula}\nFecha renta: {self.fecha_renta}\n'
+        cadena += f'Dias de renta: {self.dias_de_renta}\nEstatus: {self.estatus}'
+        return cadena
---
+++ flaskProject/alchemyClasses/Usuario.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, Integer, String, LargeBinary, Boolean
+
+from alchemyClasses import db
+
+class Usuario(db.Model):
+
+    __tablename__ = 'usuarios'
+    idUsuario = Column(Integer, primary_key=True)
+    nombre = Column(String(200))
+    apPat = Column(String(200))
+    apMat = Column(String(200), nullable=True)
+    password = Column(String(64))
+    email = Column(String(500), nullable=True, unique=True)
+    profilePicture = Column(LargeBinary, nullable=True)
+    superUser = Column(Boolean, nullable=True)
+    # renta = db.relationship("Renta", cascade="all, delete-orphan")
+    
+
+    def __init__(self, nombre, ap_pat, password, ap_mat=None,
+                 email=None, profile_picture=None, super_user=None):
+        self.nombre = nombre
+        self.apPat = ap_pat
+        self.apMat = ap_mat
+        self.password = password
+        self.email = email
+        self.profilePicture = profile_picture
+        self.superUser = super_user
+
+    def __str__(self):
+        return f'Nombre: {self.nombre}\nApellido paterno: {self.apPat}\nApellido materno: {self.apMat}\nEmail: {self.email}'
